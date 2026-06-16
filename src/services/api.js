37c7df689956eb8ee33a44abf68f6c11be8a9978
yaraemad4869 <yaraissocial1@gmail.com>
2026-06-16 17:38:30 +0300

    link to MonsterApi backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://localhost:7007/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://weather-api.runasp.net/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
